2dff2833a82153bd09337767899dd0845b830432
Joao Vitor <vitor.lira98@gmail.com>
2020-10-05 01:02:26 -0300

    Rotas autenticadas

--- src/services/AuthenticateService.ts
+++ src/services/AuthenticateService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -8,6 +9,7 @@ interface Request {
   email: string;
   password: string;
 }
+
 interface Response {
   user: User;
   token: string;
@@ -29,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, 'b7bc62b14cbcbe2093dea17a1cf490a6', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
