d694cf6522b012f404b8ff98b1d62015707eb896
Joao Vitor <vitor.lira98@gmail.com>
2020-10-04 22:12:28 -0300

    Relacionamento nos models

--- src/database/migrations/1601820417906-CreateAppointments.ts
+++ src/database/migrations/1601820417906-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1601820417906
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
