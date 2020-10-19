b430f7523c14ff2193d663db08f04b5d3dc6f0ca
Joao Vitor <vitor.lira98@gmail.com>
2020-10-18 23:41:19 -0300

    Testes de agendamento

--- src/modules/appointments/services/CreateAppointments.spec.ts
+++ src/modules/appointments/services/CreateAppointments.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../infra/typeorm/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -18,5 +19,25 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('51561661161');
   });
 
-  // it('should not be able to create a new appointment on the same time', () => {});
+  it('should not be able to create a new appointment on the same date', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      provider_id: '51561661161',
+      date: appointmentDate,
+    });
+
+    expect(
+      createAppointment.execute({
+        provider_id: '51561661161',
+        date: appointmentDate,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
