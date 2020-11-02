55271e085f029cbaf404296b6629925a1d24a7f3
Joao Vitor <vitor.lira98@gmail.com>
2020-11-02 12:13:30 -0300

    Agenda do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -3,18 +3,15 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const listProviderAppointments = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-// const appointments = await appointmentsRepository.find();
-
-//  return response.json(appointments);
-// });
-
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', listProviderAppointments.index);
 
 export default appointmentsRouter;
