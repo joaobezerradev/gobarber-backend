15ef866210c043791cd6fc27ca5d72be1f7a958e
Joao Vitor <vitor.lira98@gmail.com>
2020-11-03 11:38:55 -0300

    Estrutura de notificações

---
+++ src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,27 @@
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import { getMongoRepository, MongoRepository } from 'typeorm';
+import Notification from '../schemas/Notification';
+
+export default class NotificationsRepository
+  implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    recipient_id,
+    content,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({
+      recipient_id,
+      content,
+    });
+
+    await this.ormRepository.save(notification);
+
+    return notification;
+  }
+}
